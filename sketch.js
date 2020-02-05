9cd9873d5f92332e3b408fee3f0dbbf2beb21fc4
Your Name <you@example.com>
2020-02-05 13:13:16 +0530

    made a form

--- sketch.js
+++ sketch.js
@@ -1,42 +1,14 @@
-var ball;
+var form,player,game,gameState=0,playerCount=0;
 var database,position;
 function setup(){
     database=firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var ballposition=database.ref('ball/position');
-    ballposition.on("value",readPosition,showError);
+    game=new Game();
+    game.getState();
+    game.start();
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-10,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(10,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-10);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+10);
-    }
-    drawSprites();
+   
 }
 
-function changePosition(x,y){
-    database.ref('ball/position').set({
-        'x':position.x+x,
-        'y':position.y+y
-    })
-}
-function readPosition(data){
-    position=data.val();
-    ball.x=position.x;
-    ball.y=position.y;
-}
-function showError(){
-    console.log("error");
-}
